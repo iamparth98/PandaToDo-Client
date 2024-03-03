2d66715cb85d0d365f0cf9a0ad4454f84f3352f4
Parth Tiwari <parthtiwari98@gmail.com>
2024-03-04 00:05:36 +0530

    sidebar1

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,16 @@
 import Sidebar from "../sidebar/Sidebar";
 import TaskBox from "../taskbox/TaskBox";
+import "./main.scss";
 
 function Main() {
   return (
-    <div>
-      <Sidebar />
-      <TaskBox />
+    <div className="mainContainer">
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+      <div className="taskbox">
+        <TaskBox />
+      </div>
     </div>
   );
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,22 @@
+import "./sidebar.scss";
+
 function Sidebar() {
-  return <div className="">Sidebar</div>;
+  return (
+    <div className="container">
+      <div className="item">
+        <span>My Day</span>
+      </div>
+      <div className="item">
+        <span>Important</span>
+      </div>
+      <div className="item">
+        <span>Planned</span>
+      </div>
+      <div className="item">
+        <span>Assigned</span>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
